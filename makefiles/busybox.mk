ff90bfb0d01c1f8f84c45c6fd67cc1f073ce134a
Hayley Hughes <hayley@foxes.systems>
2023-11-23 01:03:17 +1100

    Normalise menuconfig target names

--- makefiles/busybox.mk
+++ makefiles/busybox.mk
@@ -12,8 +12,8 @@ $(BUSYBOX): stamp/build-musl stamp/build-linux-headers stamp/fetch-busybox
 	cd src/$(BUSYBOX_DIR) && $(MAKE)
 	cp src/$(BUSYBOX_DIR)/busybox build
 
-.PHONY: busyboxmenuconfig
-busyboxmenuconfig: stamp/fetch-busybox
+.PHONY: busybox_menuconfig
+busybox_menuconfig: stamp/fetch-busybox
 	cp config/busybox.config src/$(BUSYBOX_DIR)/.config
 	cd src/$(BUSYBOX_DIR) && make menuconfig
 	cp src/$(BUSYBOX_DIR)/.config config/busybox.config
